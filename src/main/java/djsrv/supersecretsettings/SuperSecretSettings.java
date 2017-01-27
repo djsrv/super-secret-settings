000e060479448fb8f12e7b2de76767c41783bafb
Dylan <dservilla@gmail.com>
2017-01-26 20:24:00 -0500

    Fix tabs/spaces issues

--- src/main/java/djsrv/supersecretsettings/SuperSecretSettings.java
+++ src/main/java/djsrv/supersecretsettings/SuperSecretSettings.java
@@ -16,15 +16,15 @@ public class SuperSecretSettings
 	@Instance(SuperSecretSettings.MOD_ID)
 	public static SuperSecretSettings instance;
 	
-    public static final String MOD_ID = "supersecretsettings";
-    public static final String VERSION = "1.0";
-    
-    public ShaderHandler shaderHandler;
-    
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-    	this.shaderHandler = new ShaderHandler(Minecraft.getMinecraft());
-        MinecraftForge.EVENT_BUS.register(new GuiOptionsEventHandler());
-    }
+	public static final String MOD_ID = "supersecretsettings";
+	public static final String VERSION = "1.0";
+	
+	public ShaderHandler shaderHandler;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		this.shaderHandler = new ShaderHandler(Minecraft.getMinecraft());
+		MinecraftForge.EVENT_BUS.register(new GuiOptionsEventHandler());
+	}
 }
--- src/main/java/djsrv/supersecretsettings/render/ShaderHandler.java
+++ src/main/java/djsrv/supersecretsettings/render/ShaderHandler.java
@@ -18,34 +18,34 @@ public class ShaderHandler {
 		this.mc = mc;
 	}
 	
-    public void activateNextShader()
-    {    	
-        if (OpenGlHelper.shadersSupported)
-        {
-            if (this.mc.getRenderViewEntity() instanceof EntityPlayer)
-            {
-            	EntityRenderer entityRenderer = this.mc.entityRenderer;
-            	ResourceLocation[] shaderResourceLocations = ReflectionHelper.getPrivateValue(EntityRenderer.class, entityRenderer, LibObfuscation.SHADERS_TEXTURES);
-            	
-            	ShaderGroup theShaderGroup = ReflectionHelper.getPrivateValue(EntityRenderer.class, entityRenderer, LibObfuscation.SHADER_GROUP);
-                if (theShaderGroup != null)
-                {
-                    theShaderGroup.deleteShaderGroup();
-                }
+	public void activateNextShader()
+	{		
+		if (OpenGlHelper.shadersSupported)
+		{
+			if (this.mc.getRenderViewEntity() instanceof EntityPlayer)
+			{
+				EntityRenderer entityRenderer = this.mc.entityRenderer;
+				ResourceLocation[] shaderResourceLocations = ReflectionHelper.getPrivateValue(EntityRenderer.class, entityRenderer, LibObfuscation.SHADERS_TEXTURES);
+				
+				ShaderGroup theShaderGroup = ReflectionHelper.getPrivateValue(EntityRenderer.class, entityRenderer, LibObfuscation.SHADER_GROUP);
+				if (theShaderGroup != null)
+				{
+					theShaderGroup.deleteShaderGroup();
+				}
 
-                int shaderIndex = ReflectionHelper.getPrivateValue(EntityRenderer.class, entityRenderer, LibObfuscation.SHADER_INDEX);
-                shaderIndex = (shaderIndex + 1) % (entityRenderer.SHADER_COUNT + 1);
-                ReflectionHelper.setPrivateValue(EntityRenderer.class, entityRenderer, shaderIndex, LibObfuscation.SHADER_INDEX);
+				int shaderIndex = ReflectionHelper.getPrivateValue(EntityRenderer.class, entityRenderer, LibObfuscation.SHADER_INDEX);
+				shaderIndex = (shaderIndex + 1) % (entityRenderer.SHADER_COUNT + 1);
+				ReflectionHelper.setPrivateValue(EntityRenderer.class, entityRenderer, shaderIndex, LibObfuscation.SHADER_INDEX);
 
-                if (shaderIndex != entityRenderer.SHADER_COUNT)
-                {
-                    entityRenderer.loadShader(shaderResourceLocations[shaderIndex]);
-                }
-                else
-                {
-                    ReflectionHelper.setPrivateValue(EntityRenderer.class, entityRenderer, null, LibObfuscation.SHADER_GROUP);
-                }
-            }
-        }
-    }
+				if (shaderIndex != entityRenderer.SHADER_COUNT)
+				{
+					entityRenderer.loadShader(shaderResourceLocations[shaderIndex]);
+				}
+				else
+				{
+					ReflectionHelper.setPrivateValue(EntityRenderer.class, entityRenderer, null, LibObfuscation.SHADER_GROUP);
+				}
+			}
+		}
+	}
 }
