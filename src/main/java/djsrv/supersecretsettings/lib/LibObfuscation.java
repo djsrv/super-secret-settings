6d200366b97d9ec74236c93116191f7761df9017
Dylan <dservilla@gmail.com>
2017-01-27 17:29:51 -0500

    Backport to 1.10

--- src/main/java/djsrv/supersecretsettings/lib/LibObfuscation.java
+++ src/main/java/djsrv/supersecretsettings/lib/LibObfuscation.java
@@ -6,7 +6,7 @@ public class LibObfuscation {
 	public static final String[] SETTINGS = new String[] { "settings", "field_146443_h", "h" };
 	
 	// EntityRenderer
-	public static final String[] SHADER_GROUP = new String[] { "theShaderGroup", "field_147707_d", "af" };
-	public static final String[] SHADERS_TEXTURES = new String[] { "SHADERS_TEXTURES", "field_147712_ad", "ag" };
-	public static final String[] SHADER_INDEX = new String[] { "shaderIndex", "field_147713_ae", "ah" };
+	public static final String[] SHADER_GROUP = new String[] { "theShaderGroup", "field_147707_d", "ab" };
+	public static final String[] SHADERS_TEXTURES = new String[] { "SHADERS_TEXTURES", "field_147712_ad", "ac" };
+	public static final String[] SHADER_INDEX = new String[] { "shaderIndex", "field_147713_ae", "ad" };
 }
